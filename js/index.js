bef9bea49fa1147d32fe6dcc16ba8dca1ba96dcd
adonProjects <support.adon@zohomail.in>
2021-08-03 20:28:12 +0530

    improved checks

--- js/index.js
+++ js/index.js
@@ -7,45 +7,62 @@ const userGuess = document.querySelector('#userGuess');
 const checkButton = document.querySelector('#check');
 const message = document.querySelector('#message');
 const attempts = document.querySelector('.counter');
+const userGuessValues = document.querySelector('.userGuessValues');
 
 // generate a random number
 let randomNumber;
 startButton.addEventListener('click' ,()=>{
     randomNumber = Math.floor(Math.random()*100) + 1;
     startButton.innerHTML = 'Number Generated'
-    startText.innerHTML = 'A random number is generated between 1 to 100';
+    startText.innerHTML = 'A random number is generated between 1 and 100';
     startButton.disabled = true;
     userGuess.disabled = false
 })
+
+
+let guessVlaueList = []
 let counter = 0
 checkButton.addEventListener('click' , ()=> {
     const userGuessValue = Number(userGuess.value)
+    userGuessValues.value = guessVlaueList.toString()
+    guessVlaueList.push(userGuessValue + " ");
     console.log(userGuessValue)
-    if (userGuessValue < 100){
-        if (userGuessValue === randomNumber){
-            message.innerHTML = `Yeee ! You have done it. The Number was ${randomNumber}`;
-            startText.innerHTML = '';
-            startButton.disabled = false;
-            userGuess.disabled = true;
-            userGuess.value = '';
-            startButton.innerHTML = 'Generate a New Number'
+    if (typeof userGuessValue === Number){
+        if (userGuessValue < 100){
+            if (userGuessValue === randomNumber){
+                message.innerHTML = `Yeee ! You have done it. The Number was ${randomNumber}`;
+                message.classList.remove('text-danger');
+                message.classList.add('text-success');
+                startText.innerHTML = '';
+                startButton.disabled = false;
+                userGuess.disabled = true;
+                userGuess.value = '';
+                startButton.innerHTML = 'Generate a New Number'
+            }
+            else if (userGuessValue > randomNumber){
+                message.innerHTML = 'Your is guess is greater than actual try with a smaller number'
+                message.classList.add('text-danger');
+                userGuess.value = '';
+                counter++
+                attempts.innerHTML = counter;
+            }
+            else if (userGuessValue < randomNumber){
+                message.innerHTML = 'Your is guess is less than actual try with a greater number';
+                message.classList.add('text-danger');
+                userGuess.value = '';
+                counter++
+                attempts.innerHTML = counter;
+            }
         }
-        else if (userGuessValue > randomNumber){
-            message.innerHTML = 'Your is guess is greater than actual try with a smaller number'
-            userGuess.value = '';
-            counter++
-            attempts.innerHTML = counter;
-        }
-        else if (userGuessValue < randomNumber){
-            message.innerHTML = 'Your is guess is less than actual try with a greater number';
-            userGuess.value = '';
-            counter++
-            attempts.innerHTML = counter;
+        else{
+            message.innerHTML = 'The number you have entered is greater than 100'
         }
     }
     else{
-        message.innerHTML = 'The number you have entered is greater than 100'
+        message.innerHTML = 'Please Enter a integer between 1 and 100';
+        userGuess.value = '';
     }
+
 })
 
 
